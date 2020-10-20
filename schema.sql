fad250880fe116f8aa4d6328638cbb69a7b8fbcc
Karen Kitchens <karen.kitchens@gmail.com>
2020-10-19 22:03:20 -0500

    Add Schema

---
+++ schema.sql
@@ -0,0 +1,62 @@
+﻿CREATE TABLE departments (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE department_employees (
+    emp_no int   NOT NULL,
+    dept_no VARCHAR   NOT NULL
+);
+
+CREATE TABLE department_managers (
+    dept_no VARCHAR   NOT NULL,
+    emp_no int   NOT NULL
+);
+
+CREATE TABLE employees (
+    emp_no int   NOT NULL,
+    emp_title_id VARCHAR   NOT NULL,
+    birth_date date   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    sex VARCHAR   NOT NULL,
+    hire_date date   NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE salaries (
+    emp_no int   NOT NULL,
+    salary int   NOT NULL
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR   NOT NULL,
+    title VARCHAR   NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (
+        title_id
+     )
+);
+
+ALTER TABLE department_employees ADD CONSTRAINT fk_department_employees_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE department_employees ADD CONSTRAINT fk_department_employees_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE department_managers ADD CONSTRAINT fk_department_managers_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE department_managers ADD CONSTRAINT fk_department_managers_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
+REFERENCES titles (title_id);
+
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
